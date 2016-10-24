016557278b4601964ef4dad9d0b597c4fbd21417
Arne Schubert <atd.schubert@gmail.com>
2016-10-24 11:55:43 +0200

    Refactor TypeScript parts

--- ts/websql-api.ts
+++ ts/websql-api.ts
@@ -1,10 +1,15 @@
-/// <reference path="../../typings/browser.d.ts" />
+/// <reference path="../typings/index.d.ts" />
 
 export interface ISpatialiteDatabaseDictionary {
     [name: string]: SpatialiteDatabase;
 }
 
-var databases: ISpatialiteDatabaseDictionary = {};
+/* tslint:disable:no-empty no-any */
+const emptyFn: (...args: any[]) => void = function (...args: any[]): void {};
+/* tslint:enable:no-empty no-any */
+export const CORDOVA_MODULE_NAME: string = 'SpatialitePlugin';
+
+const databases: ISpatialiteDatabaseDictionary = {};
 
 export interface ISpatialiteDatabaseStatic {
     new (): SpatialiteDatabase;
@@ -21,9 +26,11 @@ export interface ISpatialiteDatabaseStatic {
     ): void;
 }
 
+/* tslint:disable:interface-name */
 export interface Window {
     spatialitePlugin: ISpatialiteDatabaseStatic;
 }
+/* tslint:enable */
 
 export interface IRow {
     /* tslint:disable:no-any */
@@ -54,19 +61,43 @@ export interface ISpatialiteDatabaseOptions {
     path?: string;
 }
 
+/**
+ * @class
+ * @property {number} length - Number of rows
+ */
+    //
 export class SpatialiteRows implements IRows {
     public length: number;
+    /**
+     * Rows as array
+     * @private
+     * @memberOf SpatialiteRows
+     * @type {IRow[]}
+     */
     private rows: IRow[];
 
     constructor(data: IRow[]) {
         this.length = data.length;
         this.rows = data;
     }
+
+    /**
+     * Get a row by its number
+     * @memberOf SpatialiteRows
+     * @param {number} i - Number of the row
+     * @returns {IRow}
+     */
     public item(i: number): IRow {
         return this.rows[i];
     }
 }
 
+/**
+ * @class
+ * @property {number} rowsAffected - Number of rows that was affected by the call
+ * @property {number} insertId - ID of the insert
+ * @property {SpatialiteRows} rows - Rows as object
+ */
 export class SpatialiteResultSet implements IResultSet {
     public rowsAffected: number;
     public insertId: number;
@@ -79,7 +110,25 @@ export class SpatialiteResultSet implements IResultSet {
     }
 }
 
+/**
+ * @class
+ * @property {string} name - Name of the database
+ * @property {string} path - Path to the database
+ */
 export class SpatialiteTransaction {
+
+    /**
+     * Success callback of a database call.
+     * @callback SpatialiteTransaction~successCallback
+     * @param {SpatialiteTransaction} transaction - The transaction object itself
+     * @param {SpatialiteResultSet} data - The result-set as object
+     */
+    /**
+     * Success callback of a database call.
+     * @callback SpatialiteTransaction~errorCallback
+     * @param {SpatialiteTransaction} transaction - The transaction object itself
+     * @param {Error} error
+     */
     public name: string;
     public path: string;
 
@@ -87,6 +136,14 @@ export class SpatialiteTransaction {
         this.name = name;
     }
 
+    /**
+     * Run a SQL statement on this transaction
+     * @memberOf SpatialiteTransaction
+     * @param {string} sql - The statement you want to run. Questions marks will be replaced with the data in bindings
+     * @param {string[]} binding - Bindings for the sql statement
+     * @param {SpatialiteTransaction~successCallback} successCallback
+     * @param {SpatialiteTransaction~errorCallback} errorCallback
+     */
     public executeSql(sql: string,
                       binding: string[],
                       successCallback: (transaction: SpatialiteTransaction, data: SpatialiteResultSet) => void,
@@ -110,22 +167,57 @@ export class SpatialiteTransaction {
     }
 }
 
-/* tslint:disable:no-empty no-any */
-const emptyFn: (...args: any[]) => void = function (...args: any[]): void {};
-/* tslint:enable:no-empty no-any */
-export const CORDOVA_MODULE_NAME: string = 'SpatialitePlugin';
-
-
+/**
+ * @class
+ * @property {string} name - Name of the database
+ * @property {string} path - Path to the database
+ * @property {boolean} connected - Is the database already connected?
+ */
 export class SpatialiteDatabase {
+    /**
+     * Success callback of the opening database call.
+     * @callback SpatialiteDatabase~successCallback
+     * @param {SpatialiteDatabase} data - The result-set as object
+     */
+    /**
+     * Error callback of the opening database call.
+     * @callback SpatialiteDatabase~errorCallback
+     * @param {Error} error
+     */
+    /**
+     * Error callback of the opening database call.
+     * @callback SpatialiteDatabase~transactionCallback
+     * @param {SpatialiteTransaction} transaction
+     */
+    /**
+     * Success opening callback of the simple open call.
+     * @callback SpatialiteDatabase~successOpenCallback
+     * @param {string} status
+     */
+    /**
+     * Success statement callback.
+     * @callback SpatialiteDatabase~successStatementCallback
+     * @param {SpatialiteResultSet} resultSet
+     */
+
     public name: string;
     public path: string;
     public connected: boolean = false;
 
+    /**
+     * Open or create a Database
+     * @static
+     * @memberOf SpatialiteDatabase
+     * @param {ISpatialiteDatabaseOptions} options
+     * @param {SpatialiteDatabase~successCallback} successCallback
+     * @param {SpatialiteDatabase~errorCallback} errorCallback
+     * @returns {SpatialiteDatabase}
+     */
     public static openDatabase(
         options: ISpatialiteDatabaseOptions,
         successCallback: (data: SpatialiteDatabase) => void = emptyFn,
         errorCallback:  (data: Error) => void = emptyFn): SpatialiteDatabase {
-        
+        'use strict';
         var db: SpatialiteDatabase,
             name: string = options.name;
 
@@ -145,6 +237,14 @@ export class SpatialiteDatabase {
 
     }
 
+    /**
+     * Delete a Database
+     * @static
+     * @memberOf SpatialiteDatabase
+     * @param opts
+     * @param successCallback
+     * @param {SpatialiteDatabase~errorCallback} errorCallback
+     */
     public static deleteDatabase(
         opts: ISpatialiteDatabaseOptions,
         successCallback: (data: string) => void,
@@ -168,9 +268,22 @@ export class SpatialiteDatabase {
         this.path = opts.path;
     }
 
+    /**
+     * Create a transaction object
+     * @param {SpatialiteDatabase~transactionCallback} callback
+     */
     public transaction(callback: (data: SpatialiteTransaction) => void): void {
         callback(new SpatialiteTransaction(this.name));
     }
+
+    /**
+     * Execute a SQL statement
+     * @memberOf SpatialiteDatabase
+     * @param {string} sql - The statement you want to run. Questions marks will be replaced with the data in bindings
+     * @param {string[]} binding - Bindings for the sql statement
+     * @param {SpatialiteDatabase~successStatementCallback} successCallback
+     * @param {SpatialiteDatabase~errorCallback} errorCallback
+     */
     public executeSql(sql: string,
                       binding: string[],
                       successCallback: (data: SpatialiteResultSet) => void,
@@ -186,6 +299,13 @@ export class SpatialiteDatabase {
             /* tslint:enable:no-shadowed-variable align */
         });
     }
+
+    /**
+     * Open the database that was defined in this object
+     * @memberOf SpatialiteDatabase
+     * @param {SpatialiteDatabase~successOpenCallback} successCallback
+     * @param {SpatialiteDatabase~errorCallback} errorCallback
+     */
     public open(successCallback: (data: SpatialiteDatabase) => void, errorCallback: (data: Error) => void): void {
         cordova.exec(
             (): void => {
@@ -203,4 +323,4 @@ export class SpatialiteDatabase {
     }
 }
 
-window.spatialitePlugin = SpatialiteDatabase;
+(<any>window).spatialitePlugin = SpatialiteDatabase;
